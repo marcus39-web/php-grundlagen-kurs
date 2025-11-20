f81d69b58c2ffa550372cc9e7e28e737a895cc87
Rio <marcus.39@hotmail.de>
2025-11-20 13:10:50 +0000

    hinzugefügt und Bearbeiten-Fuunktionalität hinzugefügt

--- php_kurs_woche3/projekt_notizmanager_db/inc/functions.php
+++ php_kurs_woche3/projekt_notizmanager_db/inc/functions.php
@@ -27,3 +27,7 @@ function findNote(PDO $pdo, int $id): ?object {
     return $row ?: null;
 }
  
+function updateNote(PDO $pdo, int $id, string $title, string $content, ?int $categoryId = null): void {
+    $stmt = $pdo->prepare('UPDATE notes SET title=:t, content=:c, category_id=:cat WHERE id=:id');
+    $stmt->execute([':t' => $title, ':c' => $content, ':cat' => $categoryId, ':id' => $id]);
+}
