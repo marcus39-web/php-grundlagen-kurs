d774319308aef0349d369f7852238ed82ca9d836
Rio <marcus.39@hotmail.de>
2025-11-20 12:57:49 +0000

    hinzugefügt und Bearbeiten-Formular angelegt

--- php_kurs_woche3/projekt_notizmanager_db/inc/functions.php
+++ php_kurs_woche3/projekt_notizmanager_db/inc/functions.php
@@ -1,21 +1,29 @@
 <?php
 declare(strict_types=1);
-
+ 
 function getAllNotes(PDO $pdo):array {
     $sql = 'SELECT n.id, n.title, n.content, n.created_at, c.name AS category
-       FROM notes n
-       LEFT JOIN categories c
-         ON n.category_id = c.id
-        ORDER BY n.id DESC';
-
-        return $pdo->query($sql)->fetchAll();
+    FROM notes n
+    LEFT JOIN categories c
+        ON c.id = n.category_id
+    ORDER BY n.id DESC';
+ 
+    return $pdo->query($sql)->fetchAll();
 }
-    
+   
 function safe(string $s): string {
-    return htmlspecialchars($s, ENT_QUOTES | ENT_HTML5, 'UTF-8');
+    return htmlspecialchars($s, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
 }
-    
-function addNote(PDO $pdo, string $title, string $content, ?int $categoryID = null): void {
-    $stmt = $pdo->prepare('INSERT INTO notes (title, content, category_id) VALUES (:title, :content, :category_id)');
-    $stmt->execute([':title' => $title, ':content' => $content, ':category_id' => $categoryID ]);
+ 
+function addNote(PDO $pdo, string $title, string $content, ?int $catId = null):void {
+    $stmt = $pdo->prepare('INSERT INTO notes(title, content, category_id) VALUES (:t, :c, :cat)');
+    $stmt->execute([':t' => $title, ':c' => $content, ':cat' => $catId]);
 }
+ 
+function findNote(PDO $pdo, int $id): ?object {
+    $stmt = $pdo->prepare('SELECT * FROM notes WHERE id=:id');
+    $stmt->execute([':id' => $id]);
+    $row = $stmt->fetch();
+    return $row ?: null;
+}
+ 
