4d6adb868aa57f27adb09ebc64389ab6915ed62f
marcus <marcus@php.local>
2025-11-21 04:05:57 +0000

    Zuhause declare Fehler, alle Code abgeglichen mit dem STand von Herrn Aderhold

--- php_kurs_woche3/materialien/beispiele_pdo/inc/pdo_connect.php
+++ php_kurs_woche3/materialien/beispiele_pdo/inc/pdo_connect.php
@@ -1,14 +1,11 @@
 <?php
 declare(strict_types=1);
 
-echo '<pre>', var_dump( PDO::getAvailableDrivers() ), '</pre>';
-
 try {
-  $pdo = new PDO('mysql:host=localhost;dbname=notizmanager;charset=utf8mb4','php_user', 'qU2+8i2N&vNy_tA-)PRI]Te>', [
+  $pdo = new PDO('mysql:host='. DB_HOST .';dbname=' . DB_NAME . ';charset=utf8mb4',DB_USER , DB_PASSWORD, [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
   ]);
-  echo 'Verbindung erfolgreich';
 } catch (PDOException $e) {
   echo 'DB-Fehler: ' . htmlspecialchars($e->getMessage());
 }
