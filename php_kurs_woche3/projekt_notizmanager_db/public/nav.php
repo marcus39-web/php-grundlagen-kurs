fa3e911b3ae7160663b055577bc2d1ffc7b1af07
Rio <marcus.39@hotmail.de>
2025-11-21 09:08:12 +0000

    Abgleich nach der Pause

---
+++ php_kurs_woche3/projekt_notizmanager_db/public/nav.php
@@ -0,0 +1,20 @@
+
+<?php
+  
+?>
+<nav>
+  <div class="container">
+    <ul>
+      <li><a href="<?= $path ?>index.php">Notizen</a></li>
+      <li><a href="<?= $path ?>categ-manager.php">Kategorien</a></li>
+      <li><a href="<?= $path ?>register.php">Registrieren</a></li>
+    </ul>
+    <div class="text-muted">
+      <?php if(!empty($_SESSION['user'])): ?>
+        Eingeloggt als <strong><?= safe($_SESSION['user']) ?></strong> - <a href="<?= $path ?>logout.php">Logout</a>
+      <?php else: ?>
+        <a href="<?= $path ?>login.php">Login</a>
+      <?php endif; ?>
+    </div>
+  </div>
+</nav>
